bdf238dc3612a87d07907f8bb96b95440c213812
Azar <104105417+azarqane@users.noreply.github.com>
2025-05-19 23:38:40 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: true,
-    hmr: {
-      protocol: 'wss',
-      host: 'zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3-h3p2cmls--5173--4d9fd228.local-corp.webcontainer-api.io',
-      clientPort: 443,
-      overlay: false
-    }
+    host: true, // ← ceci est suffisant sur StackBlitz
+    // 🔴 Pas de config HMR ici
   }
 });

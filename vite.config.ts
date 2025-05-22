58f93357d20756a71a13fdf03e93c84727d19c3f
Azar <104105417+azarqane@users.noreply.github.com>
2025-05-22 23:20:46 +0200

    Fix Vite WebSocket connection issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    hmr: {
-      clientPort: 443,
-      host: 'localhost',
-      protocol: 'wss'
-    },
     watch: {
       usePolling: true
     }

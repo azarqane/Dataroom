b5f8fc40e7aa39e0fdd2884c39676cd1921d085b
Azar <104105417+azarqane@users.noreply.github.com>
2025-05-22 22:44:43 +0200

    Configuration de la durée de session Supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,12 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://raohlsmmgdtijrrcngpz.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhb2hsc21tZ2R0aWpycmNuZ3B6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NjIyNzAsImV4cCI6MjA2MzMzODI3MH0.KOJutbIDFSRKC9S7lcqfFL89_Y9U78K2y5oVnMXZyTg';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: true,
-    storageKey: 'neutvault_auth',
-    storage: window.localStorage
-  }
-});
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
